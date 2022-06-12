6cd88a8c2d271fbd6bbca8be656fabfcf26088d7
suraj0910 <gautamsuraj985@gmail.com>
2022-06-12 22:50:39 +0545

    Project initialize

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import Searchbox from './components/Searchbox';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Searchbox/>
+    <h1>lets go</h1>
+    </div>
+  );
+}
+
+export default App;
